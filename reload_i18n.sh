11cc5fea3f6649b044e553c70b1e1386707a801d
Bahadir Yagan <bahadir.yagan@gmail.com>
2014-10-08 14:09:40 +0300

    fix sudo

--- reload_i18n.sh
+++ reload_i18n.sh
@@ -7,6 +7,4 @@ cp -rT discourse-turkish-i18n-kpn/ /var/www/discourse/
 cp -f discourse-turkish-i18n-kpn/reload_i18n.sh /root/reload_i18n.sh
 chmod +x /root/reload_i18n.sh
 rm -rf discourse-turkish-i18n-kpn
-sudo su - discourse
-cd /var/www/discourse
-RAILS_ENV=production bundle exec rake assets:clobber assets:precompile
+sudo -u discourse "cd /var/www/discourse;RAILS_ENV=production bundle exec rake assets:clobber assets:precompile"
